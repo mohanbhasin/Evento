e04469509a78b343ccb0144442974b0e7bc8b7fc
aryav <104564110+aryav-hash@users.noreply.github.com>
2025-05-04 19:30:53 +0530

    feat: Wrote the logic for the register and signup page and made a function database that currently has the user table.

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
-from flask import Flask, render_template, request, redirect, url_for, make_response
+import os
+from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine, text
+from flask import Flask, render_template, request, redirect, url_for, make_response, session
 from functools import wraps
 
 app = Flask(__name__)
 
+load_dotenv()
+app.secret_key = os.urandom(24)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["CONNECTION_STRING"]
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+db = SQLAlchemy(app)
 
 def nocache(view):
     @wraps(view)
@@ -17,9 +28,68 @@ def nocache(view):
 @nocache
 @app.route('/')
 def home_page():
-    return render_template('index.html')
+    if "username" in session:
+        return render_template('dashboard.html', session=session)
+    else:
+        return redirect(url_for('login'))
 
+@nocache
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if "username" in session:
+        return redirect(url_for('home_page'))
 
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        # Check if the user exists or not
+        check_query = text("SELECT * FROM user WHERE BINARY username = :username AND password = :password")
+        with engine.connect() as connection:
+            check_result = connection.execute(check_query, {
+                "username": username,
+                "password": password
+                }).fetchone()
+            if check_result is not None:
+                session['user_id'] = check_result[0]
+                session['username'] = username
+                session['password'] = password
+                return redirect(url_for('home_page'))
+            else:
+                # User does not exist
+                return render_template('login_page.html', error="Invalid credentials. Please try again.")
+    return render_template('login_page.html')
+
+@nocache
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        # Check if the user exists or not
+        check_query = text("SELECT username FROM user WHERE BINARY username = :username")
+        with engine.connect() as connection:
+            check_result = connection.execute(check_query, {
+                "username": username
+                }).fetchone()
+            if check_result is None:
+                # Insert the new user into the database
+                insert_query = text("INSERT INTO user (username, password) VALUES (:username, :password)")
+                connection.execute(insert_query, {
+                    "username": username,
+                    "password": password
+                })
+                connection.commit()
+                return redirect(url_for('login'))
+            else:
+                # User already exists
+                return render_template('register_page.html', error="User already exists. Make sure your username is unique.")
+            
+    return render_template('register_page.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
